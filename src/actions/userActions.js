f5a36aa5c93bac97c44905c518fdc2cf9f113955
Batsaikhan Ulambayar <Batsaikhan@Sugarmaas-MacBook-Pro.local>
2020-09-29 01:21:24 -0500

    endpoint change

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,13 +1,21 @@
 import axios from 'axios'
+const BASE_URL = 'https://react-project-backend.herokuapp.com'
+const REGISTRATION_URL = `${BASE_URL}/registrations`
+const SESSIONS_URL = `${BASE_URL}/sessions`
+const LOGGGED_IN_URL = `${BASE_URL}/logged_in`
+const LOGOUT_URL = `${BASE_URL}/logout`
 
 export const fetchUserRegistration = (state) => {
     return (dispatch) => {
         dispatch({ type: 'LOADING_USER'})
-        axios.post('http://localhost:3001/registrations', {user: {
+        axios.post(REGISTRATION_URL, {user: {
                                                             email: state.email, 
                                                             password: state.password, 
                                                             password_confirmation: state.password_confirmation,
-                                                            name: state.name}}, {withCredentials: true})                                              
+                                                            name: state.name}}, 
+                                                            {withCredentials: true},
+                                                            {config: {headers: {'Access-Control-Allow-Origin': '*',}}}
+                                                            )                                              
         .then(response => {
             if (response.data.status === 'created') {
                 dispatch({ type: 'USER_REGISTRATION', user: response.data.user})
@@ -21,7 +29,7 @@ export const fetchUserRegistration = (state) => {
 export const fetchUserLogin = (state) => {
     return(dispatch) => {
         dispatch({ type: 'LOADING_USER'})
-        axios.post('http://localhost:3001/sessions', {user: {
+        axios.post(SESSIONS_URL, {user: {
                                                             email: state.email, 
                                                             password: state.password}}, {withCredentials: true})
         .then(response => {
@@ -39,7 +47,7 @@ export const fetchUserLogin = (state) => {
 export const fetchCurrentUser = () => {
     return (dispatch) => {
         dispatch({ type: 'LOADING_USER'})
-        axios.get('http://localhost:3001/logged_in', {withCredentials: true})
+        axios.get(LOGGGED_IN_URL, {withCredentials: true}, {headers: {'Access-Control-Allow-Origin': '*'}})
         .then(response => {
           if (response.data.logged_in) {
                 dispatch({ type: 'CURRENT_USER', user: response.data.user})
@@ -50,7 +58,7 @@ export const fetchCurrentUser = () => {
 export const fetchUserLogout = () => {
     return (dispatch) => {
         dispatch({ type: 'LOADING_USER'})
-        axios.get('http://localhost:3001/logout', {withCredentials: true})
+        axios.get(LOGOUT_URL, {withCredentials: true})
         .then(response => {
             dispatch({ type: 'USER_LOGOUT' })
         })
